56c4c284eefbde5950be7b216f77fa827ebfc720
Fernando Paredes <nano@fdp.io>
2013-08-20 11:51:03 -0700

    Update tmux to use erikw's tmux-powerline

--- .zsh/setopt.zsh
+++
@@ -1,5 +0,0 @@
-# ===== Basics
-setopt no_beep # don't beep on error
-
-# ===== Changing Directories
-setopt auto_cd # If you type foo, and it isn't a command, and it is a directory in your cdpath, go there.

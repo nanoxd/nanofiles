690cfe454e27a219ee261089c37902fc04de4190
Fernando Paredes <nano@fdp.io>
2014-06-17 19:04:38 -0700

    Fish: Add tmuxifier to path

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,5 +1,5 @@
 # Set paths
-set -U fish_user_paths /usr/local/sbin /usr/local/bin /usr/local/share/npm/bin /usr/local/share/python ~/bin
+set -U fish_user_paths /usr/local/sbin /usr/local/bin /usr/local/share/npm/bin /usr/local/share/python ~/bin ~/.tmuxifier/bin
 
 # Load Solarized for colors
 . $HOME/.config/fish/solarized.fish
@@ -14,6 +14,7 @@ set -x GOPATH "$HOME/Developer/Code/go"
 set -x HOMEBREW_CASK_OPTS "--appdir=/Applications" # Install location
 set -x VAGRANT_HOME /Volumes/LaCie/vagrant
 set -x VAGRANT_VMWARE_CLONE_DIRECTORY /Volumes/LaCie/vagrant/vmware_clone_dir
+set -x TMUXIFIER_LAYOUT_PATH "~/.tmux-layouts"
 
 # Source Chruby
 . /usr/local/share/chruby/chruby.fish

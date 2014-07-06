296454415441ccb22aae05fb25347b53ce98779f
Fernando Paredes <nano@fdp.io>
2014-07-05 21:48:59 -0700

    Fish: Add go's bin to path

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,5 +1,5 @@
 # Set paths
-set -U fish_user_paths /usr/local/sbin /usr/local/bin /usr/local/share/npm/bin /usr/local/share/python ~/bin ~/.tmuxifier/bin
+set -U fish_user_paths /usr/local/sbin /usr/local/bin /usr/local/share/npm/bin /usr/local/share/python ~/bin ~/.tmuxifier/bin $GOPATH/bin
 
 # Load Solarized for colors
 . $HOME/.config/fish/solarized.fish
